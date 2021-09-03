4348fe417488150c4814d10efca8f27642183d16
HAYASHI333 <88976781+HAYASHI333@users.noreply.github.com>
2021-09-03 20:36:17 +0900

    修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,8 @@
 class MessagesController < ApplicationController
   def index
-  @message = Message.new
-  @room = Room.find(params[:room_id])
-  @messages = @room.messages.includes(:user)
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -15,4 +15,11 @@ class MessagesController < ApplicationController
       render :index
     end
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
